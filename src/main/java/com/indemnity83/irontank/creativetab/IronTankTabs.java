e90e00cd4ada222be1e63397627b8fd72f2da0da
Kyle Klaus <kklaus@indemnity83.com>
2015-09-21 18:37:16 -0700

    fix(creative tab): Localize the creative tab name

--- src/main/java/com/indemnity83/irontank/creativetab/IronTankTabs.java
+++ src/main/java/com/indemnity83/irontank/creativetab/IronTankTabs.java
@@ -11,18 +11,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class IronTankTabs {
-	public static final CreativeTabs MainTab = new CreativeTabs(Reference.MODID)
+	public static final CreativeTabs MainTab = new CreativeTabs(Reference.MODID.toLowerCase())
     {
         @Override
         public Item getTabIconItem()
         {
             return new ItemStack(ModBlocks.ironTank, 1).getItem();
-        }
-
-        @Override
-        public String getTranslatedTabLabel()
-        {
-            return "Iron Tanks";
         }
     };
 }
