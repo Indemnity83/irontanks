c7f970f5ca5b5a02c61e4bd47e13546a0e354edf
Kyle Klaus <kklaus@indemnity83.com>
2017-12-21 15:22:24 -0800

    fix(tank): tanks don't lose capacity between world saves
    Tank capacity is now stored in the nbt data so it can be re-loaded properly between games saves.

--- src/main/java/com/indemnity83/irontanks/common/tiles/TankTile.java
+++ src/main/java/com/indemnity83/irontanks/common/tiles/TankTile.java
@@ -1,10 +1,28 @@
 package com.indemnity83.irontanks.common.tiles;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fluids.Fluid;
 
 public class TankTile extends buildcraft.factory.tile.TileTank {
     public TankTile withCapacity(int capacity) {
         this.tank.setCapacity(capacity * Fluid.BUCKET_VOLUME);
         return this;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+
+        if (compound.hasKey("Capacity")) {
+            tank.setCapacity(compound.getInteger("Capacity"));
+        }
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+        super.writeToNBT(compound);
+        compound.setInteger("Capacity", tank.getCapacity());
+
+        return compound;
     }
 }
