bce717f059dcfc7beac8c4c91ffb917fead24ac3
Kyle Klaus <kklaus@indemnity83.com>
2026-06-19 11:30:26 -0700

    Give each tank tier its own hardness and blast resistance

--- src/main/java/com/indemnity83/irontank/reference/TankType.java
+++ src/main/java/com/indemnity83/irontank/reference/TankType.java
@@ -24,18 +24,18 @@ import cpw.mods.fml.common.registry.GameRegistry;
  *
  */
 public enum TankType{
-    IRON(32, "irontank", Arrays.asList("ingotIron", "ingotRefinedIron"), Arrays.asList("tgtg0gtgt", "gggt4tggg"), 25/3F),
-    GOLD(48, "goldtank", Arrays.asList("ingotGold"), Arrays.asList("tgtg1gtgt", "gggt5tggg"), 25/3F),
-    DIAMOND(64, "diamondtank", Arrays.asList("gemDiamond"), Arrays.asList("gggt2tggg", "gggg5gttt"), 25/3F),
-    COPPER(27, "coppertank", Arrays.asList("ingotCopper"), Arrays.asList("tgtg0gtgt"), 25/3F),
-    SILVER(43, "silvertank", Arrays.asList("ingotSilver"), Arrays.asList("tgtg4gtgt", "gggt1tggg"), 25/3F),
-    OBSIDIAN(64, "obsidiantank", Arrays.asList("obsidian"), Arrays.asList("tgtg3gtgt"), 6000000.0F),
-    GLASS(0, "", Arrays.asList("blockGlass"), Arrays.asList(""), 0f),
-	EMERALD(96, "emeraldtank", Arrays.asList("gemEmerald"), Arrays.asList("gggt3tggg"), 25/3F),
-	ALUMINIUM(96, "aluminiumtank", Arrays.asList("ingotAluminium", "ingotAluminum"), Arrays.asList("gggt3tggg"), 25/3F),
-	STAINLESSSTEEL(128, "stainlesssteeltank", Arrays.asList("ingotStainlessSteel"), Arrays.asList("gggt8tggg", "gggt7tggg"), 25/3F),
-	TITANIUM(256, "titaniumtank", Arrays.asList("ingotTitanium"), Arrays.asList("gggt9tggg"), 25/3F),
-	TUNGSTENSTEEL(512, "tungstensteeltank", Arrays.asList("ingotTungstenSteel"), Arrays.asList("gggtztggg"), 25/3F);
+    IRON(32, "irontank", Arrays.asList("ingotIron", "ingotRefinedIron"), Arrays.asList("tgtg0gtgt", "gggt4tggg"), 3.0F, 5.0F),
+    GOLD(48, "goldtank", Arrays.asList("ingotGold"), Arrays.asList("tgtg1gtgt", "gggt5tggg"), 4.0F, 7.0F),
+    DIAMOND(64, "diamondtank", Arrays.asList("gemDiamond"), Arrays.asList("gggt2tggg", "gggg5gttt"), 6.0F, 8.0F),
+    COPPER(27, "coppertank", Arrays.asList("ingotCopper"), Arrays.asList("tgtg0gtgt"), 2.0F, 4.0F),
+    SILVER(43, "silvertank", Arrays.asList("ingotSilver"), Arrays.asList("tgtg4gtgt", "gggt1tggg"), 5.0F, 6.0F),
+    OBSIDIAN(64, "obsidiantank", Arrays.asList("obsidian"), Arrays.asList("tgtg3gtgt"), 1200.0F, 50.0F),
+    GLASS(0, "", Arrays.asList("blockGlass"), Arrays.asList(""), 0.3F, 0.3F),
+	EMERALD(96, "emeraldtank", Arrays.asList("gemEmerald"), Arrays.asList("gggt3tggg"), 6.0F, 8.0F),
+	ALUMINIUM(96, "aluminiumtank", Arrays.asList("ingotAluminium", "ingotAluminum"), Arrays.asList("gggt3tggg"), 4.0F, 5.0F),
+	STAINLESSSTEEL(128, "stainlesssteeltank", Arrays.asList("ingotStainlessSteel"), Arrays.asList("gggt8tggg", "gggt7tggg"), 8.0F, 9.0F),
+	TITANIUM(256, "titaniumtank", Arrays.asList("ingotTitanium"), Arrays.asList("gggt9tggg"), 10.0F, 10.0F),
+	TUNGSTENSTEEL(512, "tungstensteeltank", Arrays.asList("ingotTungstenSteel"), Arrays.asList("gggtztggg"), 14.0F, 12.0F);
 
 	/**
 	 * fluid capacity of the tank type
@@ -63,12 +63,18 @@ public enum TankType{
 	 */
 	public final float resistance;
 
-	TankType(int capacity, String name, List<String> materials, List<String> recipes, float resistance) {
+	/**
+	 * Hardness (mining time) of the tank type
+	 */
+	public final float hardness;
+
+	TankType(int capacity, String name, List<String> materials, List<String> recipes, float resistance, float hardness) {
 		this.capacity = capacity;
 		this.name = name;
 		this.materials = new ArrayList<String>();
 		this.recipes = new ArrayList<String>();
 		this.resistance = resistance;
+		this.hardness = hardness;
 
 		this.materials.addAll(materials);
 		this.recipes.addAll(recipes);
